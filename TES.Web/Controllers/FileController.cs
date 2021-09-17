dd17c432de69d25fb0012ed6cf17ec62712e1661
Mind <mindkriauciunas@gmail.com>
2021-09-17 15:37:38 +0300

    FileController/Git ignore removed.

---
+++ TES.Web/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TES.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: api/Test/getTest/Guid
+        [HttpGet]
+        [Route("Download/{filePath}")]
+        public FileResult FileDownload(string filePath)
+        {
+            const string DefaultContentType = "application/octet-stream";
+
+            var provider = new FileExtensionContentTypeProvider();
+
+            if (!provider.TryGetContentType(filePath, out string contentType))
+            {
+                contentType = DefaultContentType;
+            }
+
+            string path = Path.Combine(_webHostEnvironment.ContentRootPath, $"Uploads/{filePath}");
+            return File(System.IO.File.ReadAllBytes(path), contentType, "Solution.txt");
+        }
+    }
+}
